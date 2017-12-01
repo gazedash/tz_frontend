4b444bf26faa0b9f8e18e16dc9df979efcc23ccb
Andrey <skrtroot@protonmail.com>
2017-12-01 17:07:42 +0300

    Add Common components (styled A and router's Link)

---
+++ src/components/Common.js
@@ -0,0 +1,9 @@
+import styled, { withComponent } from "styled-components";
+import { Link } from "react-router-dom";
+
+export const A = styled.a`
+  text-decoration: none;
+  color: #5555cc;
+`;
+
+export const LinkStyled = A.withComponent(Link);
