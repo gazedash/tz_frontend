465ed1f3f361408feaebc13b5bb6642473dcc1a1
Andrey <skrtroot@protonmail.com>
2017-12-01 17:08:39 +0300

    Update UserList component

--- src/components/UserList.js
+++ src/components/UserList.js
@@ -2,14 +2,17 @@
 import React, { Component } from "react";
 import UserCard from "./UserCard";
 import mockUsers from "../data/mockUsers.json";
+
 class UserList extends Component {
   static defaultProps = {
-    items: mockUsers
+    items: mockUsers,
+    onClick: () => {}
   };
+
   render() {
     return <div>
     {this.props.items.map((item) => (
-        <UserCard key={item.id} item={item} />
+        <UserCard key={item.id} item={item} onClick={() => this.props.onClick(item.id)} />
     ))}
     </div>;
   }
