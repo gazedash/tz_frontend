d0a696c016bd58adf1aaa0b783f0984254ea5d0f
Andrey <skrtroot@protonmail.com>
2017-12-01 17:08:24 +0300

    Update UserPhotos component

--- src/components/UserPhotos.js
+++ src/components/UserPhotos.js
@@ -1,14 +1,33 @@
 // @flow
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import styled from "styled-components";
+import mockPhotos from "../data/mockPhotos.json";
+
+const Flex = styled.div`
+    display: flex;
+    flex-wrap: wrap;
+`
+const Img = styled.img`
+    display: block;
+    width: 150;
+    height: 150;
+`
 
 class UserPhotos extends Component {
-    render() {
-        return (
-            <div>
-                
-            </div>
-        );
-    }
+  static defaultProps = {
+    items: mockPhotos
+  };
+  render() {
+    return (
+      <Flex>
+        {this.props.items.map(item => (
+        //   <div key={item.thumbnailUrl}>
+            <Img key={item.thumbnailUrl} alt="title" src={item.thumbnailUrl} />
+        //   </div>
+        ))}
+      </Flex>
+    );
+  }
 }
 
 export default UserPhotos;
