3c14dec6faafe412c5615e0be5cd157cd79d8acf
Andrey <skrtroot@protonmail.com>
2017-12-01 17:09:27 +0300

    Update UserAlbums: render albums

--- src/components/UserAlbums.js
+++ src/components/UserAlbums.js
@@ -1,14 +1,25 @@
 // @flow
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import {A} from "./Common";
+import mockAlbums from "../data/mockAlbums.json";
 
 class UserAlbums extends Component {
-    render() {
-        return (
-            <div>
-                
-            </div>
-        );
-    }
+  static defaultProps = {
+    items: mockAlbums
+  };
+  render() {
+    return (
+      <div>
+        Albums {/* JSON.stringify(this.props) */}
+        {this.props.items.map(({ id, title, userId }) => (
+          <A key={id} href={"/thumb/" + id}>
+            <div>{title}</div>
+            <div>{id} {userId}</div>
+          </A>
+        ))}
+      </div>
+    );
+  }
 }
 
 export default UserAlbums;
