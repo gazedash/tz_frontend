350849a798dd6a42fc01394e78699fa532f27020
Andrey <skrtroot@protonmail.com>
2017-12-03 14:21:10 +0300

    Update UserList, add onMount hook

--- src/components/UserList.js
+++ src/components/UserList.js
@@ -1,20 +1,30 @@
 // @flow
 import React, { Component } from "react";
 import UserCard from "./UserCard";
-import mockUsers from "../data/mockUsers.json";
 
 class UserList extends Component {
   static defaultProps = {
-    items: mockUsers,
-    onClick: () => {}
+    items: [],
+    onClick: () => {},
+    onMount: () => {}
   };
 
+  componentDidMount() {
+    this.props.onMount();
+  }
+
   render() {
-    return <div>
-    {this.props.items.map((item) => (
-        <UserCard key={item.id} item={item} onClick={() => this.props.onClick(item.id)} />
-    ))}
-    </div>;
+    return (
+      <div>
+        {this.props.items.map(item => (
+          <UserCard
+            key={item.id}
+            item={item}
+            onClick={() => this.props.onClick(item.id)}
+          />
+        ))}
+      </div>
+    );
   }
 }
 
