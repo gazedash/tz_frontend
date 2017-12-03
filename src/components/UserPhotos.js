cad45e545e132f8077fe59b71f684b2b4980cb20
Andrey <skrtroot@protonmail.com>
2017-12-03 14:18:34 +0300

    Update UserPhotos, add onMount hook, remove mock data

--- src/components/UserPhotos.js
+++ src/components/UserPhotos.js
@@ -1,33 +1,49 @@
 // @flow
 import React, { Component } from "react";
-import styled from "styled-components";
-import mockPhotos from "../data/mockPhotos.json";
+import styled, { keyframes } from "styled-components";
+import { withRouter } from "react-router-dom";
+import * as R from "ramda";
 
 const Flex = styled.div`
-    display: flex;
-    flex-wrap: wrap;
-`
+  display: flex;
+  flex-wrap: wrap;
+`;
+
+const opacity = keyframes`
+from {
+  opacity: 0;
+}
+to {
+  opacity: 1;
+}
+`;
 const Img = styled.img`
-    display: block;
-    width: 150;
-    height: 150;
-`
+  display: block;
+  width: 150;
+  height: 150;
+  animation: ${opacity} 1s linear 1;
+`;
 
 class UserPhotos extends Component {
   static defaultProps = {
-    items: mockPhotos
+    items: [],
+    onMount: () => {}
   };
+  componentDidMount() {
+    const id = R.pathOr(null, ["match", "params", "id"], this.props);
+
+    this.props.onMount(id);
+  }
+
   render() {
     return (
       <Flex>
         {this.props.items.map(item => (
-        //   <div key={item.thumbnailUrl}>
-            <Img key={item.thumbnailUrl} alt="title" src={item.thumbnailUrl} />
-        //   </div>
+          <Img key={item.thumbnailUrl} alt="" src={item.thumbnailUrl} />
         ))}
       </Flex>
     );
   }
 }
 
-export default UserPhotos;
+export default withRouter(UserPhotos);
