105d3dc0dd96e48b367fa597bc25475d53df0571
Andrey <skrtroot@protonmail.com>
2017-12-03 14:42:06 +0300

    Add Common card styled-component

--- src/components/Common.js
+++ src/components/Common.js
@@ -6,4 +6,13 @@ export const A = styled.a`
   color: #5555cc;
 `;
 
+export const Card = A.extend`
+display: flex;
+box-shadow: 0 3px 6px rgba(0, 0, 0, 0.3);
+transition: background-color 0.5s;
+&:hover {
+  background-color: #eee;
+}
+`;
+
 export const LinkStyled = A.withComponent(Link);
