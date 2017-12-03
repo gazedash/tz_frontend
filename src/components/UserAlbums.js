f1521413d7582fb8ebf6f8c9c3f9dcc9f8a12847
Andrey <skrtroot@protonmail.com>
2017-12-03 14:19:41 +0300

    Update UserAlbums, add onMount hook, remove mock data

--- src/components/UserAlbums.js
+++ src/components/UserAlbums.js
@@ -1,20 +1,34 @@
 // @flow
 import React, { Component } from "react";
-import {A} from "./Common";
-import mockAlbums from "../data/mockAlbums.json";
+import { A } from "./Common";
+import { withRouter } from "react-router-dom";
+import * as R from "ramda"
 
 class UserAlbums extends Component {
   static defaultProps = {
-    items: mockAlbums
+    items: [],
+    onClick: () => {},
+    onMount: () => {}
   };
+  componentDidMount() {
+    const id = R.pathOr(null, ['match', 'params', 'id'], this.props)
+    
+    this.props.onMount(id);
+  }
   render() {
     return (
       <div>
-        Albums {/* JSON.stringify(this.props) */}
+        Albums
         {this.props.items.map(({ id, title, userId }) => (
-          <A key={id} href={"/thumb/" + id}>
+          <A
+            onClick={() => this.props.onClick(id)}
+            key={id}
+            href={"/thumb/" + id}
+          >
             <div>{title}</div>
-            <div>{id} {userId}</div>
+            <div>
+              {id} {userId}
+            </div>
           </A>
         ))}
       </div>
@@ -22,4 +36,4 @@ class UserAlbums extends Component {
   }
 }
 
-export default UserAlbums;
+export default withRouter(UserAlbums);
